43e7cb20069338fd0737c89dc02916244d3b3879
Hector Valdes <hectorvaldes0808@gmail.com>
2023-12-08 17:12:44 -0600

    feat: move SendMessage to Chat Component

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,5 +1,5 @@
 // Components
-import SendMessage from 'components/SendMessage';
+import SendMessage from './SendMessage';
 import Messages from './Messages';
 
 // Styles
@@ -11,6 +11,7 @@ const Chat = () => {
     return (
         <ChatWrapper className='card bg-base-300 shadow-xl mx-8 my-4 h-full'>
             <div className="card-body mx-8">
+                <h2 className='card-title'>Chat</h2>
                 <Messages />
                 <SendMessage />
             </div>
